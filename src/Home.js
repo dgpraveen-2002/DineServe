39556f6f4f2a1b01f88c606e5014da00f9c14bf1
dgpraveen_2002 <praveendaggubatti7658@gmail.com>
2025-03-05 22:55:38 +0530

    first commit

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+          <center>
+            <h4>Home </h4>
+          </center>
+    </div>
+  );
+}
+
+export default Home;
